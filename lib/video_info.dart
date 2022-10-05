9506c83fe7290d258b9eb6d2971f7205cdddd45c
Rajat Sharma <97823698+Rajat119@users.noreply.github.com>
2022-10-05 14:24:48 +0530

    Add files via upload

--- lib/video_info.dart
+++ lib/video_info.dart
@@ -14,7 +14,9 @@ class VideoInfo extends StatefulWidget {
 class _VideoInfoState extends State<VideoInfo> {
   List videoInfo=[];
   bool _playArea=false;
-  late VideoPlayerController _controller;
+  bool _isPlaying=false;
+  bool _disposed=false;
+  VideoPlayerController? _controller;
   _initData() async {
     await DefaultAssetBundle.of(context).loadString("json/videoinfo.json").then((value){
       setState((){
@@ -27,6 +29,16 @@ class _VideoInfoState extends State<VideoInfo> {
     super.initState();
     _initData();
   }
+
+  @override
+  void dispose(){
+    _disposed=true;
+    _controller?.pause();
+    _controller?.dispose();
+    _controller=null;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -153,6 +165,7 @@ class _VideoInfoState extends State<VideoInfo> {
                     ),
                   ),
                   _playView(context),
+                  _controlView(context),
                 ],
               ),
             ),
@@ -196,29 +209,107 @@ class _VideoInfoState extends State<VideoInfo> {
       )
     );
   }
+
+  Widget _controlView(BuildContext context){
+    return Container(
+      height: 120,
+      width: MediaQuery.of(context).size.width,
+      color: color.AppColor.gradientSecond,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FlatButton(onPressed: ()async{
+
+          },
+              child: Icon(Icons.fast_rewind,size: 36, color: Colors.white)),
+
+          FlatButton(onPressed: ()async{
+            if(_isPlaying){
+              setState(() {
+                _isPlaying=false;
+              });
+              _controller?.pause();
+            }else{
+              setState(() {
+                _isPlaying=true;
+              });
+              _controller?.play();
+            }
+          },
+              child: Icon( _isPlaying?Icons.pause: Icons.play_arrow,size: 36, color: Colors.white)),
+
+          FlatButton(onPressed: ()async{
+
+          },
+              child: Icon(Icons.fast_forward,size: 36, color: Colors.white))
+        ],
+      ),
+    );
+  }
+  
   Widget _playView(BuildContext context){
     final controller =_controller;
     if(controller!=null&&controller.value.isInitialized){
-      return Container(
-        height: 300,
-        width: 300,
+      return AspectRatio(
+        aspectRatio: 16/9,
         child: VideoPlayer(controller),
       );
     }else{
-      return Text("Being Initiali");
+      return AspectRatio(
+          aspectRatio: 16/9,
+          child: Center(child: Text("Preparing",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white60,
+          ))));
     }
   }
-  _onTapVideo(int index){
-    final controller = VideoPlayerController.network(videoInfo[index]["videoUrl"]);
-    _controller=controller;
-    setState(() {
-    });
-    controller..initialize().then((_){
-      controller.play();
-      setState(() {
-      });
-    });
+  var _onUpdateControllerTime;
+  void _onControllerUpdate()async{
+    if(_disposed){
+      return;
+    }
+    _onUpdateControllerTime=0;
+    final now = DateTime.now().millisecondsSinceEpoch;
+    if(_onUpdateControllerTime>now){
+      return;
+    }
+
+    _onUpdateControllerTime=now+500;
+
+    final controller = _controller;
+    if(controller==null){
+      debugPrint("controller is null");
+      return;
+    }
+    if(!controller.value.isInitialized){
+      debugPrint("controller cannot be initialized");
+    return;
+    }
+    final playing = controller.value.isPlaying;
+    _isPlaying=playing;
   }
+  _initializeVideo(int index) async{
+    final controller = VideoPlayerController.network(videoInfo[index]["videoUrl"]);
+    final old = _controller;
+    _controller = controller;
+    if(old!=null){
+      old.removeListener(_onControllerUpdate);
+      old.pause();
+    }
+    setState(() {});
+    controller
+      ..initialize().then((_){
+        old?.dispose();
+        controller.addListener(_onControllerUpdate);
+        controller.play();
+        setState(() {});
+      });
+  }
+  _onTapVideo(int index){
+    _initializeVideo(index);
+  }
+
   _listView(){
     return ListView.builder(
         padding: EdgeInsets.symmetric(horizontal: 30, vertical: 8),
